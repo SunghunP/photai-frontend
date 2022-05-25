efbcd049b2321d2f0a4ab75a4ed19f3a993fb5ce
Joe Park <sunghun.josephp@gmail.com>
2022-05-24 17:12:53 -0700

    Create routes and link their respective components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import './App.css';
+// Import components 
+import Header from './components/Header';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Menu from './pages/Menu';
+import Contact from './pages/Contact';
+import About from './pages/About';
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/contact" element={<Contact />}/>
+        <Route path="/about" element={<About/>}/>
+      </Routes>
     </div>
   );
 }
