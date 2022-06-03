e505e7a2121de9aec82afc5271bebc5ccf9859b5
Joe Park <sunghun.josephp@gmail.com>
2022-06-03 11:00:01 -0700

    Rename file path to fix netlify error

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -10,5 +10,4 @@ export default function Menu() {
   }
 
   return <div> {menuItems ? loading() : loaded()}</div>
-  
 }
--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,3 +1,4 @@
 export default function Contact() {
   return <div>This is the Contact Page</div>
+  // hi
 }
