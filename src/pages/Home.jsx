4a31e16df5085be6ecec42cfb7bc88930da46766
Jihunp <edwardpark.park@gmail.com>
2022-06-10 11:37:20 -0700

    creted wrapper and container for grid

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,6 @@
+import styled from "styled-components";
+import React from 'react';
+
 export default function Home({ menu, setMenu }) {
 
 
@@ -6,7 +9,29 @@ export default function Home({ menu, setMenu }) {
 
   return (
     <div>
+      <StyledContainer>
+        <GridWrapper>
+          <div><img src="" alt=""></img></div>
+          <div>
+            <h1>Pho Tai Puyallup</h1>
+            <p>Hello this is a p tag and this is used to describe some of the great food that pho tai has</p>
+          </div>
+        </GridWrapper>
+      </StyledContainer>
+  
       <button></button>
     </div>
   )
 }
+
+const StyledContainer = styled.div`
+  margin: auto;
+  width: 50%;
+  border: 3px solid white;
+  padding: 80px;
+  text-align: center;
+`
+const GridWrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+`
