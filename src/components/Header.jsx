0ebeb44b872348f042f99e720a46be1781c61e46
Jihunp <edwardpark.park@gmail.com>
2022-06-09 22:29:31 -0700

    testing

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom"
 export default function Header() {
   return (
     <nav>
+      <p>What's popping</p>
       <Link to="/">HOME</Link>
       <Link to="/menu">MENU</Link>
       <Link to="/about">ABOUT</Link>
