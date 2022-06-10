74ba29313e36a133804dcbc8ca6659fd277db129
Joe Park <sunghun.josephp@gmail.com>
2022-06-10 11:33:29 -0700

    Change foodType to foodtype

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -6,19 +6,22 @@ export default function Menu({ menu }) {
     return <h1>Loading...</h1>
   };
 
+  const menuItems = ['appetizer', 'special combo tray', 'noodle salad bowl', 'rice plate', 'pho', 'grilled and wok', 'fried rice', 'banh mi', 'coffee', 'iced drinks', 'bubble juice', 'side orders']
+
   function loaded() {
     return (
-      <div>
+      <div className="menu">
         {menu.map((menuItem, idx) => 
           <MenuItem 
             key={menuItem._id}
             idx={idx}
+            itemNumber={menuItem.itemNumber}
             menuItemTag={menuItem.menuItemTag}
             name={menuItem.name} 
             englishName={menuItem.englishName}
             desc={menuItem.desc}
             price={menuItem.price}
-            foodType={menuItem.foodType}
+            foodtype={menuItem.foodtype}
           />
         )}
       </div>
