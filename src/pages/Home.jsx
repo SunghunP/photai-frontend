bcee3d3666ebdaf22a39f5e6819e22b346590ebf
Jihunp <edwardpark.park@gmail.com>
2022-06-16 21:29:32 -0700

    created some styling for home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import React from 'react';
+import woodbg from '../images/woodbg.png'
 
 export default function Home({ menu, setMenu }) {
 
@@ -11,13 +12,18 @@ export default function Home({ menu, setMenu }) {
     <div>
       <StyledContainer>
         <GridWrapper>
-          <div><img src="" alt=""></img></div>
+          <div>
+            <img className="home-soup-image" src={woodbg} alt="piece of wood"></img>
+          </div>
           <div>
             <h1>Pho Tai Puyallup</h1>
-            <p>Hello this is a p tag and this is used to describe some of the great food that pho tai has</p>
+            <p>Hello this is a p tag and this is used to describe some of the great food that pho tai has blah blah blah babel babel this is a test to see how a lot of text makes my box container look</p>
           </div>
         </GridWrapper>
       </StyledContainer>
+      <div>This is where the carasoul goes</div>
+      <div>This is the google maps 
+      </div>
   
       <button></button>
     </div>
@@ -27,11 +33,19 @@ export default function Home({ menu, setMenu }) {
 const StyledContainer = styled.div`
   margin: auto;
   width: 50%;
-  border: 3px solid white;
   padding: 80px;
   text-align: center;
+
+// size image to fit on the left side of the grid
+.home-soup-image{
+  float: left;
+  height: 100%;
+  width: 90%;
+  border-radius: 85%;
+}
 `
+
 const GridWrapper = styled.div`
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  grid-template-columns: repeat(2, 1fr);
 `
