f1c077a83fd73fdc42b15cb14495ebf02613e514
Joe Park <sunghun.josephp@gmail.com>
2022-06-17 12:13:58 -0700

    Add styling to menu items to make them more visible

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -1,7 +1,21 @@
-export default function MenuItem({ idx, itemNumber, menuItemTag, name, englishName, desc, price, foodtype}) {
+import styled from "styled-components";
+
+const StyledP = styled.p`
+  font-size: 30px;
+  background: rgba(40,40,40, 0.8);
+  color: white; 
+  padding: 10px; 
+  border-radius: 100px;
+  :hover {
+    background: rgba(12,12,12, 1);
+    color:#B4DBB8;
+  }
+`
+
+export default function MenuItem({ idx, itemNumber, menuItemTag, name, englishName, desc, price, foodtype }) {
   return <div className="menuItems">
-    <p key={idx} id={itemNumber} foodtype={foodtype}>
+    <StyledP key={idx} id={itemNumber} foodtype={foodtype}>
       {menuItemTag} {englishName} {name} {price}
-    </p>
+    </StyledP>
   </div>
 }
